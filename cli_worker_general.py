07347c40b94010003036e3a92170689c2560378f
mouad157 <94315381+mouad157@users.noreply.github.com>
2024-04-23 11:16:26 +0800

    Add files via upload

--- cli_worker_general.py
+++ cli_worker_general.py
@@ -7,7 +7,7 @@ from rich.console import Console
 from src.filter import Filter
 from src.caller import OpenAICaller
 from src.evaluator import LCSEvaluator, SDASEvaluator
-from src.utils import get_product_mapping, get_swv_mapping, save_results, banner, config_generator, get_sr_data
+from src.utils import get_product_mapping, get_swv_mapping, save_results, banner, config_generator, get_sr_data, get_price, create_summary
 
 def general_worker(console, caller, config):
     with console.status("[bold green] Loading software version mapping file...") as status:
@@ -29,17 +29,27 @@ def general_worker(console, caller, config):
         filter = Filter(config,console)
         time.sleep(3)
         console.log(f'filter loaded.')
-    
+    garbage = list()
     with console.status("[bold green]Retrieving metadata from the file...") as status:
         if config['test_sr'] == []:
             mnodes = cm
         else:
-            mnodes = {x:cm[x] for x in config['test_sr']}
+            mnodes = {}
+            for x in config['test_sr']:
+                try:
+                    mnodes[x] = cm[x]
+                #mnodes = {x:cm[x] for x in config['test_sr']}
+                except:
+                    console.log(f"The SR with ID: {x} does not exist in the metadata file provided, and will be ignored")
+                    garbage.append(x)
+                    pass
         time.sleep(3)
         console.log(f"Got {len(mnodes)} nodes from the file.")
     
     results = list()
     total, correct_sum, correct_sum_p = 0, 0, 0
+    total_price_p , total_price_sw = 0 ,0
+    total_price_reduced_p , total_price_reduced_sw = 0 ,0
     valid_total = 0
     with console.status("[bold green] Node Processing...") as status:
         for index, mnode in enumerate(mnodes):
@@ -58,6 +68,7 @@ def general_worker(console, caller, config):
             p_str = "/ ".join(p_list)
 
             notes = list()  
+            p_notes = list()
             sw_notes = list()
             nnodes = cn[mnode]
             
@@ -68,39 +79,56 @@ def general_worker(console, caller, config):
                     note_content = nnode["extracted_note"]
                 else:
                     note_content=nnode['Note__c']
-                if filter.filter_by_local_classifier(note_content, s_list):
+                if filter.filter_by_local_classifier(note_content, s_list,False):
                     sw_notes +=[note_content]
+                if filter.filter_by_local_classifier(note_content, p_list,True):
+                    p_notes +=[note_content]
                 notes += [note_content]
-
+                
             if len(sw_notes) == 0:
                 sw_notes=  notes
-
-            doc, doc_sw = "" , ""
+            
+            doc, doc_sw, doc_p = "" , "", ""
 
 
             if config["include_notes"]:
                 doc += "\n".join(notes)
                 doc_sw += "\n".join(sw_notes)
+                doc_p += "\n".join(p_notes)
             for field in config["fields"]:
                 doc += "\n" + mnode_data[field]
                 doc_sw += "\n" + mnode_data[field]
+                doc_p += "\n" + mnode_data[field]
 
 
 
             if config['mcq']:
                 question = f'Which product is principally discussed in these documents?'
                 console.log(f"Current node {mnode_sr} [{index+1}/{len(mnodes)}]")
-                response_p, price_p, num_tokens_p = caller.product_analysis([doc], question, p_str, status)
+                response_p, price_p, num_tokens_p = caller.product_analysis([doc_p], question, p_str, status)
             else:
                 question = f'What product is principally discussed in these documents?'
                 console.log(f"Current node {mnode_sr} [{index+1}/{len(mnodes)}]")
-                response_p, price_p, num_tokens_p = caller.product_analysis([doc], question, None, status)
+                response_p, price_p, num_tokens_p = caller.product_analysis([doc_p], question, None, status)
             
             # question = f'Which software version of {mnode_product} is principally discussed in these documents? Answer "None" if you cannot find answer in the given documents.'
             question = f'Which software version is principally discussed in these documents?'
             
             response, price_sw, num_tokens_sw = caller.software_analysis([doc_sw], question, s_str, status)
+
+            price_original_sw, num_tokens_original_sw = get_price(config["model_name"],f'Question: {question} \n \
+                    The ONLY software version should be selected from the given software version list: {s_str} \
+                    Response in this JSON format: \n {{"software_version": "","explanation": "", "summary": ""}} \n {doc}')
             
+            price_original_p, num_tokens_original_p = get_price(config["model_name"],f'Question: {question} \n \
+                    The ONLY product name should be selected from the given product name list: {p_str} \
+                    Response in this JSON format: \n {{"product_name": "","explanation": "", "summary": ""}} \n {doc}')
+            
+            total_price_sw = total_price_sw + price_original_sw
+            total_price_p = total_price_p + price_original_p
+            total_price_reduced_sw = total_price_reduced_sw + price_sw
+            total_price_reduced_p = total_price_reduced_p + price_p
+
             prediction, summary, explanation = "", "", ""
             try:
                 response_json = json.loads(response)
@@ -122,7 +150,7 @@ def general_worker(console, caller, config):
                 prediction_p = response_p
 
             if config["eval"]:
-                valid_sr = mnode_swv in doc
+                valid_sr = mnode_swv in doc_p
                 correct = SDASEvaluator.eval(swv_map[mnode_swv], prediction, 0.2)
                 correct_p, overlap = LCSEvaluator.eval(mnode_product, prediction_p, 0.5)
                 total += 1
@@ -130,7 +158,7 @@ def general_worker(console, caller, config):
                 correct_sum += correct
                 correct_sum_p += correct_p
                 
-                console.log(f'Ground truth: [bold green]{mnode_swv}[/bold green] Prediction: [bold yellow]{prediction}[/bold yellow] Valid: {str(valid_sr)} Passed: {str(correct)}')
+                console.log(f'Ground truth: [bold green]{swv_map[mnode_swv]}[/bold green] Prediction: [bold yellow]{prediction}[/bold yellow] Valid: {str(valid_sr)} Passed: {str(correct)}')
                 console.log(f'Explanation: [i]{explanation}[/i]')
                 console.log(f'Ground truth: [bold green]{mnode_product}[/bold green] Prediction: [bold yellow]{prediction_p}[/bold yellow] Overlap: {overlap} Passed: {str(correct_p)}')
                 console.log(f'Explanation: [i]{explanation_p}[/i]')
@@ -146,6 +174,8 @@ def general_worker(console, caller, config):
                     "correct_sw": correct,
                     "num_tokens_sw": num_tokens_sw,
                     "price_sw" : price_sw,
+                    "original_price_sw" : price_original_sw,
+                    "original_num_tokens_sw" : num_tokens_original_sw,
                     "prediction_p": prediction_p,
                     "explanation_p": explanation_p,
                     "summary_p": summary_p,
@@ -153,6 +183,8 @@ def general_worker(console, caller, config):
                     "correct_p": correct_p,
                     "num_tokens_p": num_tokens_p,
                     "price_p" : price_p,
+                    "original_price_p" : price_original_p,
+                    "original_num_tokens_p" : num_tokens_original_p,
                 }]
                 
             else: 
@@ -163,11 +195,15 @@ def general_worker(console, caller, config):
                     "summary_sw": summary,
                     "num_tokens_sw": num_tokens_sw,
                     "price_sw" : price_sw,
+                    "original_price_sw" : price_original_sw,
+                    "original_num_tokens_sw" : num_tokens_original_sw,
                     "prediction_p": prediction_p,
                     "explanation_p": explanation,
                     "summary_p": summary_p,
                     "num_tokens_p": num_tokens_p,
                     "price_p" : price_p,
+                    "original_price_p" : price_original_p,
+                    "original_num_tokens_p" : num_tokens_original_p,
                 }]
                 console.log(f' Prediction: [bold yellow]{prediction}[/bold yellow]')
                 console.log(f'Explanation: [i]{explanation}[/i]')
@@ -181,12 +217,28 @@ def general_worker(console, caller, config):
         save_results(pd.DataFrame.from_dict(results), save_file_path)
         time.sleep(3)
         if config["eval"]:
-            console.log(f"Well done. There are {valid_total} valid instances. We have {correct_sum} correct predictions. Please check the detailed result at '{save_file_path}'.")
-            accuracy_sw = correct_sum / len(mnodes)
-            accuracy_p = correct_sum_p / len(mnodes)
-            console.log(f"The accuracy for the software version prediction is {accuracy_sw}, and the accuracy for the product name prediction is {accuracy_p}")
+            console.log(f"Well done. There are {valid_total} valid instances. We have {correct_sum} correct predictions out of the {len(mnodes)} SRs. Please check the detailed result at '{save_file_path}'.")
+            if len(garbage)!=0:
+                console.log(f"Found in the test_sr list invalid SR(s) that are not mentioned in the metadata file: {garbage}.")
+            accuracy_sw = round(100*correct_sum / len(mnodes),2)
+            accuracy_p = round(100*correct_sum_p / len(mnodes),2)
+            console.log(f"The accuracy for the software version prediction is {accuracy_sw}%, and the accuracy for the product name prediction is {accuracy_p}%")
+            console.log(f"Using the filter saved us {round(total_price_p-total_price_reduced_p,3)}$ on product name prediction, and {round(total_price_sw-total_price_reduced_sw,3)}$ on software version prediction. \
+                        These saving translate to {round(100*(total_price_p-total_price_reduced_p)/total_price_p,2)}% of the original cost of product name prediction, and {round(100*(total_price_sw-total_price_reduced_sw)/total_price_sw,2)}% of the original cost of software version prediction. ")
+            conetent = f"Well done. There are {valid_total} valid instances. We have {correct_sum} correct predictions out of the {len(mnodes)} SRs. Please check the detailed result at '{save_file_path}'. \n\
+Found in the test_sr list invalid SR(s) that are not mentioned in the metadata file: {garbage}. \n\
+The accuracy for the software version prediction is {accuracy_sw}%, and the accuracy for the product name prediction is {accuracy_p}% \n\
+Using the filter saved us {round(total_price_p-total_price_reduced_p,3)}$ on product name prediction, and {round(total_price_sw-total_price_reduced_sw,3)}$ on software version prediction.  \n\
+These saving translate to {round(100*(total_price_p-total_price_reduced_p)/total_price_p,2)}% of the original cost of product name prediction, and {round(100*(total_price_sw-total_price_reduced_sw)/total_price_sw,2)}% of the original cost of software version prediction.\n "
+            create_summary('summary.txt', conetent)
+
         else:
-            console.log(f" Please check the detailed result at '{save_file_path}'.")
+            console.log(f"Using the filter saved us {round(total_price_p-total_price_reduced_p,3)}$ on product name prediction, and {round(total_price_sw-total_price_reduced_sw,3)}$ on software version prediction. these saving translate to {round(100*(total_price_p-total_price_reduced_p)/total_price_p,2)}% of the original cost of product name prediction, and {round(100*(total_price_sw-total_price_reduced_sw)/total_price_sw,2)}% of the original cost of software version prediction. ")
+            console.log(f"Please check the detailed result at '{save_file_path}'.")
+            conetent = f"Using the filter saved us {round(total_price_p-total_price_reduced_p,3)}$ on product name prediction, and {round(total_price_sw-total_price_reduced_sw,3)}$ on software version prediction.  \n \
+These saving translate to {round(100*(total_price_p-total_price_reduced_p)/total_price_p,2)}% of the original cost of product name prediction, and {round(100*(total_price_sw-total_price_reduced_sw)/total_price_sw,2)}% of the original cost of software version prediction.\n \
+Please check the detailed result at '{save_file_path}'."
+            create_summary('summary.txt', conetent)
 
 if __name__ == "__main__":
     console = Console(record=True)
